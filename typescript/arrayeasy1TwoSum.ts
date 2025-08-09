115a891eca55459cdc3dae4a9809d99900cc31e5
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2025-08-09 13:39:12 +0900

    2025/08/09

---
+++ typescript/arrayeasy1TwoSum.ts
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length; i++)
+    for (let j = i + 1; j < nums.length; j++)
+      if (nums[i] + nums[j] === target) return [i, j];
+  return [];
+}
+// @lc code=end
